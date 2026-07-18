32df51f9a083447d4d8a7391529567e604756a69
crisca807 <crisca807@gmail.com>
2026-07-18 08:23:53 -0500

    Eliminar base para Vercel

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/portafolio/" : "/",
+export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
   ],
-}));
+});
